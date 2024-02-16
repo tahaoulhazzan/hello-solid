eee34b835772003c88299384be8f7335cf5843a2
Taha Oulhazzan <taha.oulhazzan@gmail.com>
2024-02-16 16:04:23 +0100

    je m'appelle

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 function App() {
   return (
-    <p class="text-4xl text-green-700 text-center py-20">Hello Les Mineurs!</p>
+    <p class="text-4xl text-green-700 text-center py-20">Hello Les Mineurs!<br/> Je m'appelle Taha</p>
   );
 }
 
